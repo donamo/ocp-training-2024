9cbdc3b3c62d4e206a26537f5baf2aef3b20e364
Ambrus Szabo <szabo.ambrus@mbhbank.hu>
2024-10-07 15:10:27 +0200

    word-picker v1.1

--- word-picker/word_picker.py
+++ word-picker/word_picker.py
@@ -1,7 +1,7 @@
 from random import choice
 from flask import Flask
 app = Flask(__name__)
-choices = ["first", "second", "third"]
+choices = ["Sheldon Cooper", "Leonard Hofstadter", "Penny", "Howard Wolowitz", "Rajesh Koothrappali", "Amy Farrah Fowler", "Bernadette Rostenkowski", "Stuart Bloom", "Beverly Hofstadter", "Mary Cooper", "Leslie Winkle", "Priya Koothrappali", "Barry Kripke", "Emily Sweeney", "Zack Johnson"]
 @app.route("/")
 def pick_word():
   return choice(choices)
